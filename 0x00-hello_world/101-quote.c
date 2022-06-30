4cac7050150292b299c45edda872cd503a78dfcd
lemakavely <lemakavely@gmail.com>
2022-06-30 01:33:09 -0700

    optional two trial 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:	1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n");
+		return (1);
+}
