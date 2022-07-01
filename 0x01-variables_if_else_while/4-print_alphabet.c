4b3b6439f1fa90ccd861dfcd3a59e2c8251173f2
lemakavely <lemakavely@gmail.com>
2022-07-01 09:01:52 -0400

    print alphabets without 'q' and 'e' program

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'z'; x++)
+	{
+		x = tolower(x);
+		if(x == 'q' || x == 'e')
+		{
+		continue;
+		}
+		else
+		{
+		putchar(x);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
