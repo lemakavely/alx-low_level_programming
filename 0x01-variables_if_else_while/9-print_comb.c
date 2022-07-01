a6dd77f921796297b20719be346139dfc5260539
lemakavely <lemakavely@gmail.com>
2022-07-01 13:28:20 -0400

    task 9 print numbers with ',' and ' 'wq

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x % 10 + '0');
+		if (x == 9)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');			
+		}
+	}
+
+	//putchar('$');
+	putchar('\n');
+	return (0);
+}
