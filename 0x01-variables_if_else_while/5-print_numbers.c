31580aaf5b3a7be457a058a447f8a0f85d5f4a5d
lemakavely <lemakavely@gmail.com>
2022-07-01 09:11:19 -0400

    print decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+
+	putchar('\n');
+	return (0);
+}
