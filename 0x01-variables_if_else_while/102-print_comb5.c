bbd5b45f5730b65487209715f3dc79f8f0f554a7
lemakavely <lemakavely@gmail.com>
2022-07-01 15:39:23 -0400

    optional 3 printing 2 digit 99 numbers combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+	int i;
+	int j;
+	int k;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			for (j = x; j <= 9; j++)
+			{
+				for (k = i + 1; k <= 9; k++)
+				{
+
+					putchar(x % 10 + '0');
+					putchar(i % 10 + '0');
+					putchar(' ');
+					putchar(j % 10 + '0');
+					putchar(k % 10 + '0');
+
+					if (x == 9 && i == 8)
+					{
+					continue;
+
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
