592b25bb7e4decf4826a606b55069df26f1205e0
lemakavely <lemakavely@gmail.com>
2022-07-05 14:45:24 -0400

    0. _putchar print _putchar trial 1 (betty satisfied header created and included and header include guard added)

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * _putchar - prints _putchar
+ *
+ * Return: always 0
+ */
+
+int _putchar(void)
+{
+	printf("_putchar");
+	return (0);
+}
+
+#endif
