6471b46511425e7cb79f56f0e0595b4ebfef4dbf
lemakavely <lemakavely@gmail.com>
2022-07-06 07:34:23 -0400

    trial for task 10 trial 1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add to numbers
+ * @a: operand 1
+ * @b: operand 2
+ *
+ * Return: n
+ */
+
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
