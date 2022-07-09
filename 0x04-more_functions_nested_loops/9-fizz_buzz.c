e86f41facd1ef470a06d758f5fcb3d8e613dd3a0
lemakavely <lemakavely@gmail.com>
2022-07-09 14:02:50 -0400

    task 9 trial 6 using std

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,22 +1,4 @@
 #include <stdio.h>
-#include "_putchar.c"
-
-
-/**
- * print - print multiple digits
- * @n: to be printed
- *
- * Return: none
- */
-
-void print(int n)
-{
-	if (n / 10)
-	{
-		print(n / 10);
-	}
-	_putchar(n % 10 + '0');
-}
 
 /**
  * main - the fizz buzz code
@@ -32,38 +14,23 @@ int main(void)
 	{
 		if (!(i % 3) && !(i % 5))
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('c');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
+			printf("%s", "FizzBuzz");
 		}
 		else if (!(i % 3) && i % 5)
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
+			printf("%s", "Fizz");
 		}
 		else if (!(i % 5) && i % 3)
 		{
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
+			printf("%s", "Buzz");
 		}
 		else
 		{
-			if (i / 10)
-			print(i);
-			else
-				_putchar(i + '0');
+			printf("%d", i);
 		}
 		if (i != 100)
-		_putchar(' ');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
