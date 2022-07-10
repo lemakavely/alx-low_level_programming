8407bafb8b9dbd4b8bd839564628160e357a4454
lemakavely <lemakavely@gmail.com>
2022-07-10 07:16:35 -0400

    task 102 optional trial 3

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	int k;
-	int i = 0;
-	int j = 1;
-	long int sum = i + j;
+	unsigned long i = 0;
+	unsigned long j = 1;
+	unsigned long sum = i + j;
 
 	for (k = 3; k <= 52; ++k)
 	{
-		printf("%ld\n", sum);
+		printf("%lu", sum);
 		i = j;
 		j = sum;
 		sum = i + j;
