14ed09c0efbb85358fe5d8097db860fdc8f87f3e
lemakavely <lemakavely@gmail.com>
2022-07-10 07:35:31 -0400

    task 104 optional trial 1

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int k;
+	unsigned long i = 0;
+	unsigned long j = 1;
+	unsigned long sum = i + j;
+
+	for (k = 3; k <= 100; ++k)
+	{
+		if (k == 100)
+			printf("%lu", sum);
+		else
+		{
+		printf("%lu, ", sum);
+		i = j;
+		j = sum;
+		sum = i + j;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
