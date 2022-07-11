ad15ebcd6fcecb02f52ed7a8977bc5466c3f5d56
lemakavely <lemakavely@gmail.com>
2022-07-11 04:36:22 -0400

    task 3 pointers trial 1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string
+ * @str: string to bee coomputed
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	char *p = str;
+
+	for (; *p != '\0'; p++)
+	{
+		putchar(*p);
+	}
+}
