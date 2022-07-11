912147daa21a4b28f59699e8b1078a6645ddf6b2
lemakavely <lemakavely@gmail.com>
2022-07-11 03:40:20 -0400

    task 1 pointers trial 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swap value of a and b
+ * @a: operand1
+ * @b: operand2
+ *
+ * Return: none
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
