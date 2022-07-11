1198909a19628f949d3b10818bd16e9466d25650
lemakavely <lemakavely@gmail.com>
2022-07-11 06:41:25 -0400

    task 8 pointers trial 2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_array - print out array
+ * @a: array pointer
+ * @n: array size to print
+ * Return: none
+ */
+
 void print_array(int *a, int n)
 {
 	int i;
