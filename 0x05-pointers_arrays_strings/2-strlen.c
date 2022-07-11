dc46c2e19d330a7d687050f6401502f904b0cb9b
lemakavely <lemakavely@gmail.com>
2022-07-11 04:26:03 -0400

    task 2 pointers trial 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+
+/**
+ * _strlen - returns string length
+ * @s: string to bee coomputed
+ *
+ * Return: string_length
+ */
+
+int _strlen(char *s)
+{
+	char *p = s;
+
+	for (; *p != '\0'; p++)
+	{
+	}
+
+	return (p - s);
+}
