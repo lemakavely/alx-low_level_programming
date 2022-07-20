cca170cde54d9a753a9cb623ffa676e3b0543cb5
lemakavely <lemakavely@gmail.com>
2022-07-20 00:17:11 -0400

    task 5 trial 1

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - char occurance check
+ * @haystack: memory area
+ * @needle: data to be checked for occurance
+ *
+ * Return: haystack pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int j;
+	unsigned int p;
+	unsigned int i = 0;
+
+	p = strlen(haystack);
+	for (j = 0; j < p; j++)
+	{
+		if (needle[i] == haystack[j])
+		break;
+	}
+	if (needle[i] == haystack[j])
+		return (haystack + j);
+	return (0);
+}
