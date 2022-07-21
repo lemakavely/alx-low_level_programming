d8e3df188dcd6261601e49378496210bef7e12e3
lemakavely <lemakavely@gmail.com>
2022-07-21 14:09:36 -0400

    task 0 trial 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string
+ * @s: string to be printed
+ *
+ * Return: none
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] && s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+this is the directory of the project recursion
