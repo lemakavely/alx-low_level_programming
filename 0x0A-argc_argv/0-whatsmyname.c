91cde303a1df5069cdd2e816fd194232073685a9
lemakavely <lemakavely@gmail.com>
2022-07-22 18:32:48 -0400

    task 0 argc and argv trial 3

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * Return: argc
  */
 
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	printf("%s", argv[0]);
-	return (argc);
+	printf("%s\n", argv[0]);
+	return (0);
 }
