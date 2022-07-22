730ee5fe519135c2fa8cab0be089ceec47e2be26
lemakavely <lemakavely@gmail.com>
2022-07-22 01:48:13 -0400

    task 1 trial 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s: string to be printed
+ *
+ * Return: none
+ */
+
+void _print_rev_recursion(char *s)
+{
+	
+	if (s[0] == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(s[0]);
+}
