6b89adb9b9862af8e758ddc171680d9318b8b7e6
lemakavely <lemakavely@gmail.com>
2022-07-22 18:39:39 -0400

    task 1 argc and argv trial 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments entered to prompt
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
