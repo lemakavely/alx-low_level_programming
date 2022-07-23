a626c2648d3831eb7c8d42a062f9da1924b166d3
lemakavely <lemakavely@gmail.com>
2022-07-23 15:31:03 -0400

    task 4 argc and argv trial 4

--- 0x0A-argc_argv/4-adder_before.c
+++ 0x0A-argc_argv/4-adder_before.c
@@ -10,26 +10,22 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int i;
-	int j;
 	int sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
+		if (atoi(argv[i]))
 		{
-			if (isdigit(argv[i][j]) == 0)
-			{
-				printf("%s\n", "Error");
-				return (1);
-			}
+			sum += atoi(argv[i]);
 		}
-	}
-	for (i = 1; i < argc; i++)
-	{
-		sum += atoi(argv[i]);
+		else
+		{
+			puts("Error");
+			return (1);
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
