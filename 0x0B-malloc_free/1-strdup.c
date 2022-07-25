bea052571ca190ceee81a28cb75c2e6a83222c3e
lemakavely <lemakavely@gmail.com>
2022-07-25 16:03:53 -0400

    task 1 malloc free trial 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - copy string to new pointer
+ * @str: string pointer as a parameter
+ *
+ * Return: pointer to the array or null
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int i, j;
+
+	j = strlen(str);
+	arr = malloc(j);
+
+	if (j == 0 || arr == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+	{
+		arr[i] = str[i];
+	}
+	return (arr);
+}
