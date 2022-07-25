426db5762a1810b331786a15e1e68396b8fe8830
lemakavely <lemakavely@gmail.com>
2022-07-25 15:44:25 -0400

    task 0 malloc free trial 2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	if (size == 0)
-		return (NULL);
 	arr = malloc(size);
-		for (i = 0; i < size; i++)
-		{
-			arr[i] = c;
-		}
+	if (size == 0 || arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
 	return (arr);
 }
