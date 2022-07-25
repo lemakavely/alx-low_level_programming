319b10bd2d46bcb104b86466757b0c6dad893f69
lemakavely <lemakavely@gmail.com>
2022-07-25 00:42:09 -0400

    task 6 pointers mnamn trial 1

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * cap_string - converts string word initials to uppercase
+ * @a: string input
+ * Return: string pointer
+ */
+
+char *cap_string(char *a)
+{
+	int i = 0, len;
+
+	len = strlen(a);
+	for (i = 0; i <= len && a[i]; i++)
+	{
+		if (a[i] == ' ' || a[i] == '	' || a[i] == ',')
+			a[i + 1] = toupper(a[i + 1]);
+		if (a[i] == '\n' || a[i] == ';' || a[i] == '.')
+			a[i + 1] = toupper(a[i + 1]);
+		if (a[i] == '!' || a[i] == '?' || a[i] == '"')
+			a[i + 1]	= toupper(a[i + 1]);
+		if (a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}')
+			a[i + 1] = toupper(a[i + 1]);
+	}
+	return (a);
+}
