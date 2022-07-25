dba3b90c27189ef031928ec4b48cecf62d358717
lemakavely <lemakavely@gmail.com>
2022-07-25 17:35:28 -0400

    task 2 malloc free trial 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,32 +13,43 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
-	int i, j, k, m;
+	int i, j = 0, k = 0, m;
 
-	if (s1 == NULL && s2 != NULL)
-		j = 0;
-	else if (s2 == NULL && s1 != NULL)
-		k = 0;
-	else if (s1 == NULL && s2 == NULL)
-	{
-		j = 0;
-		k = 0;
-	}
-	else
+	if (s1 != NULL && s2 != NULL)
 	{
 		j = strlen(s1);
 		k = strlen(s2);
 	}
+	else if (s1 == NULL && s2 != NULL)
+		k = strlen(s2);
+	else if (s2 == NULL && s1 != NULL)
+		j = strlen(s1);
+	else
+	{
+		k = 0;
+		j = 0;
+	}
 	arr = malloc(j + k + 1);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < j; i++)
+	if (s1 == NULL && s2 != NULL)
 	{
-		arr[i] = s1[i];
+		for (i = 0; i < k; i++)
+			arr[i] = s2[i];
 	}
-	for (i = j, m = 0; i < j + k; i++, m++)
+	else if (s2 == NULL && s1 != NULL)
 	{
-		arr[i] = s2[m];
+		for (i = 0; i < j; i++)
+			arr[i] = s1[i];
+	}
+	else if (s1 == NULL && s2 == NULL)
+		*arr = '\0';
+	else
+	{
+		for (i = 0; i < j; i++)
+			arr[i] = s1[i];
+		for (i = j, m = 0; i < j + k; i++, m++)
+			arr[i] = s2[m];
 	}
 	return (arr);
 }
