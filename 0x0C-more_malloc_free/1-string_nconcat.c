34878385ca14d33b7da7248319c3a7be562b9295
lemakavely <lemakavely@gmail.com>
2022-07-28 15:06:33 -0400

    task 1 more malloc free trial 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,28 +3,45 @@
 #include <string.h>
 
 /**
- * string_nconcat - concatenate strings
- * @s1: first string
- * @s2: second string
- * @n: size of s2 to concatenate
+ * malloc_size - finds size for malloc
+ * @s1: string one input
+ * @s2: string two input
+ * @n: size of s2 to concat
  *
- * Return: pointer to the created memory or NULL
+ * Return: len
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+int malloc_size(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int i = 0, j = 0, len0, len1, len;
+	unsigned int len0, len1, len;
 
-	len0 = strlen(s1);
-	len1 = strlen(s2);
+	if (s1 == NULL)
+		len0 = 0;
+	else
+		len0 = strlen(s1);
+	if (s2 == NULL)
+		len1 = 0;
+	else
+		len1 = strlen(s2);
 	if (len1 < n)
 		len = len0 + len1 + 1;
 	else
 		len = len0 + n + 1;
-	ptr = malloc(len);
-	if (ptr == NULL)
-		return (NULL);
+	return (len);
+}
+
+/**
+ * first_string - fill pointer
+ * @s1: first string
+ * @s2: second string
+ * @ptr: pointer to concatenated string
+ * @n: size of s2 to concat
+ * Return: pointer to concatenated string
+ */
+
+char *first_string(char *s1, char *s2, char *ptr, unsigned int n)
+{
+	unsigned int i = 0, j = 0;
 
 	while (*s1 != '\0')
 	{
@@ -40,5 +57,52 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	ptr[i] = '\0';
+	return (ptr);
+}
+
+
+/**
+ * string_nconcat - concatenate strings
+ * @s1: first string
+ * @s2: second string
+ * @n: size of s2 to concatenate
+ *
+ * Return: pointer to the created memory or NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	ptr = malloc(malloc_size(s1, s2, n));
+	if (ptr == NULL)
+		return (NULL);
+	if (s1 != NULL && s2 != NULL)
+	{
+	ptr = first_string(s1, s2, ptr, n);
+	}
+	else if (s1 != NULL && s2 == NULL)
+	{
+	while (*s1 != '\0')
+	{
+		ptr[i] = *s1;
+		s1++;
+		i++;
+	}
+	ptr[i] = '\0';
+	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+	while (*s2 != '\0' && i < n)
+	{
+		ptr[i] = *s2;
+		s2++;
+		i++;
+	}
+	ptr[i] = '\0';
+	}
+	else
+		ptr[i] = '\0';
 	return (ptr);
 }
