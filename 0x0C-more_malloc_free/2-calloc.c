77c90de5af1cb8681650b1fa4148c03806f7d783
lemakavely <lemakavely@gmail.com>
2022-07-29 15:22:21 -0400

    task 2 more malloc free trial 1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: array element size
+ * @size: size of the pointer in bytes
+ *
+ * Return: pointer to the created memory or NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL || nmemb == 0 || size == 0)
+		return (NULL);
+	return (ptr);
+}
