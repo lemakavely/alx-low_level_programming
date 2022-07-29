6aea1643ea61740265acda78d0dab14ae2d2069e
lemakavely <lemakavely@gmail.com>
2022-07-29 17:20:24 -0400

    task 4 optional more malloc free trial 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,23 +14,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 	char *ptr0 = ptr;
-	unsigned int i = 0, j = new_size;
+	unsigned int i = 0;
 
-	if (new_size == 0 && ptr != NULL)
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	else if (new_size == old_size)
+		return (ptr);
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
-	if (ptr == NULL)
-		return (p);
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size > old_size)
-		j = old_size;
-	for (i = 0; i < j; i++)
+	for (i = 0; i < new_size && i < old_size; i++)
 		*(p + i) = *(ptr0 + i);
 	free(ptr);
 	return (p);
