27f560af8cbd641be119785c0bb7aaac34559d7b
lemakavely <lemakavely@gmail.com>
2022-07-29 15:37:52 -0400

    task 2 more malloc free trial 5

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,10 +14,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i = 0;
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL || nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	for (i = 0; i < nmemb * size; i++)
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
 		ptr[i] = 0;
 	return (ptr);
 }
