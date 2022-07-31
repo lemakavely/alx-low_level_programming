83e4bf1fc099fc003aec8b8d0b51616f87b77a80
lemakavely <lemakavely@gmail.com>
2022-07-31 16:19:23 -0400

    task 6 pointers arrays and strings trial 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -15,3 +18,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
+
+int _putchar(char c);
+
+#endif
