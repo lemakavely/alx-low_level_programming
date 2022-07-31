d218e94d02754fa2e3e6096068ed6d9dffc773b2
lemakavely <lemakavely@gmail.com>
2022-07-31 11:20:45 -0400

    task 6 optional malloc free trial 4

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -56,17 +56,21 @@ int charcnt(char *s1, int i)
 char **strtow(char *str)
 {
 	char **arr;
-	int i = 0, p = 0, j = 0, k = 0, l = 0, m = 0, n = 0, o = 0;
+	int i = 0, q = 0, p = 0, j = 0, k = 0, l = 0, m = 0, n = 0, o = 0;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
+	for (q = 0; str[q]; q++)
+		;
+	if (q == 1 && str[0] == ' ')
+		return (NULL);
 	m = wrdcnt(str);
-	arr = malloc(sizeof(char*) * (m + 1));
+	arr = malloc(sizeof(char *) * (m + 1));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < m; i++)
 	{
-		arr[i] = (char *)malloc(sizeof(char) * (charcnt(str, i) + 1));
+		arr[i] = (char *)malloc(sizeof(char) * (charcnt(str, i)));
 		if (arr[i] == NULL)
 		{
 			for (p = 0; p < m; p++)
