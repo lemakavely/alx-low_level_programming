5f3726bc55aa0779b80662db897674a8a2e0828c
lemakavely <lemakavely@gmail.com>
2022-07-31 17:31:26 -0400

    task 8 optional pointers arrays and strings trial 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - sets a pointer to a char
+ * @s: pointer to take
+ * @to: pointer to set
+ *
+ * Return: none
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
