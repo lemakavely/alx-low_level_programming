4d8bfeb57c02eeafa1a8cec99edbaea7a58cbe94
lemakavely <lemakavely@gmail.com>
2022-08-01 08:36:07 -0400

    task 4 structures and typedef trial 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,9 +13,8 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog new_dog1;
 	unsigned int i = 0, j = 0, k = 0, l = 0;
-	dog_t *new_dog0 = &new_dog1;
+	dog_t *new_dog0;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
@@ -30,7 +29,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog0);
 		return (NULL);
 	}
-	for (k = 0; k < i; k++)
+	for (k = 0; k < i + 1; k++)
 		new_dog0->name[k] = name[k];
 	new_dog0->age = age;
 	new_dog0->owner = malloc(j + 1);
@@ -40,7 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog0);
 		return (NULL);
 	}
-	for (l = 0; l < j; l++)
+	for (l = 0; l < j + 1; l++)
 		new_dog0->owner[l] = owner[l];
 	return (new_dog0);
 }
