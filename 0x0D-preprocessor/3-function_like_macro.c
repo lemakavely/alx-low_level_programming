b033ff77a3823aaea7ae113948f8d85795752035
lemakavely <lemakavely@gmail.com>
2022-08-01 14:52:46 -0400

    task 3 preprocessor trial 1

---
+++ 0x0D-preprocessor/3-function_like_macro.c
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#include "3-function_like_macro.h"
+
+int lemakavely = 0;
+
+#define ABS(x) ((x < 0) ? (x * -1) : x)
+
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+
+#define FUNCTION_LIKE_MACRO_H
+
+int lemakavely = 0;
+
+#define ABS(x) ((x < 0) ? (x * -1) : x)
+
+#endif
