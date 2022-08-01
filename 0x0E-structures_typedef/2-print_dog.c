d3fe0199e026e90c868cd878ab1255bac8b2e20e
lemakavely <lemakavely@gmail.com>
2022-08-01 08:16:11 -0400

    all tasks of structures and typedef trial 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,16 +12,16 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
-		printf("Name: (nil)");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("Age: (nil)");
-	else
+	else 
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)");
+		else
+			printf("Name: %s\n", d->name);
 		printf("Age: %.6f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: (nil)");
-	else
-		printf("Owner: %s\n", d->owner);
+		if (d->owner == NULL)
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,13 +14,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog new_dog1;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
 	dog_t *new_dog0 = &new_dog1;
 
-	new_dog0 = malloc(strlen(name) + strlen(owner) + sizeof(float));
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	new_dog0 = malloc(sizeof(dog_t));
 	if (new_dog0 == NULL)
 		return (NULL);
-	new_dog0->name = name;
+	i = strlen(name);
+	j = strlen(owner);
+	new_dog0->name = malloc(i);
+	if (new_dog0->name == NULL)
+	{
+		free(new_dog0);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		new_dog0->name[k] = name[k];
 	new_dog0->age = age;
-	new_dog0->owner = owner;
+	new_dog0->owner = malloc(j);
+	if (new_dog0->owner == NULL)
+	{
+		free(new_dog0->name);
+		free(new_dog0);
+		return (NULL);
+	}
+	for (l = 0; l < j; l++)
+		new_dog0->owner[l] = owner[l];
 	return (new_dog0);
 }
