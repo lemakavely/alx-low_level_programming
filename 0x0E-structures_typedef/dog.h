ac5d2e71349865e8f54a7fc580020b685535df9a
lemakavely <lemakavely@gmail.com>
2022-08-01 04:06:37 -0400

    task 0 structures and typedef trial 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - structure for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
