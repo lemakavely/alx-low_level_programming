d5d82d5424b18a4935f8b0a91ee478c8b6d341ef
lemakavely <lemakavely@gmail.com>
2022-08-02 01:00:52 -0400

    task 104 fibonacci last trial

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,31 @@ int main(void)
 {
 	int k;
 	unsigned long i = 0;
-	unsigned long j = 1;
+	unsigned long j = 1, a, b, c, d, e, f, l;
 	unsigned long sum = i + j;
 
-	for (k = 3; k <= 100; ++k)
+	for (k = 3; k < 89; ++k)
 	{
-		if (k == 100)
-			printf("%lu", sum);
-		else
-		{
 		printf("%lu, ", sum);
 		i = j;
 		j = sum;
 		sum = i + j;
-		}
 	}
+	a = j / 1000000000;
+	b = j % 1000000000;
+	c = sum / 1000000000;
+	d = sum % 1000000000;
+	for (l = 89; l < 100; l++)
+	{
+		printf("%lu%lu, ", c, d);
+		e = a;
+		f = b;
+		a = c;
+		b = d;
+		c = e + a + ((f + b) / 1000000000);
+		d = (f + b) % 1000000000;
+	}
+	printf("%lu%lu\n", c, d);
 	putchar('\n');
 	return (0);
 }
