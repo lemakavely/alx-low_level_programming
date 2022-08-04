ea4fe0040acd8afdd9fb94845283354db4916f20
lemakavely <lemakavely@gmail.com>
2022-08-04 16:19:56 -0400

    task 1 variadic function trial 3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  * print_numbers - prints numbers given
@@ -17,16 +16,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list lemak;
 
 	va_start(lemak, n);
-	if (n != 0 && separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i != n - 1)
-				printf("%d%s", va_arg(lemak, int), separator);
-			else
-				printf("%d", va_arg(lemak, int));
-		}
-		va_end(lemak);
+		if (i != n - 1)
+			printf("%d%s", va_arg(lemak, int), separator);
+		else
+			printf("%d", va_arg(lemak, int));
 	}
+	va_end(lemak);
 	printf("\n");
 }
