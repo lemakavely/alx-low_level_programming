14bcd55c018c4c2fef788e43a0917ed53dcbf600
lemakavely <lemakavely@gmail.com>
2022-08-04 16:08:37 -0400

    task 2 variadic function trial 3

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list lemak;
 	char *s;
-	
+
 	va_start(lemak, n);
 	for (i = 0; i < n; i++)
 	{
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+this is the directory of variadic function projects
