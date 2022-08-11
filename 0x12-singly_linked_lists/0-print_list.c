9bc178a1eb58750228c954748b549fb7a91b61d8
lemakavely <lemakavely@gmail.com>
2022-08-11 16:16:03 -0400

    task 0 singly linked list project trial 2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	const list_t *temp;
+	unsigned int i = 0;
 
-	while (h)
+	temp = h;
+	while (temp)
 	{
-		if (h->str == NULL)
+		if (temp->str == NULL)
 			printf("[%u] %s\n", 0, "(nil)");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s\n", temp->len, temp->str);
 		i++;
-		h = h->next;
+		temp = temp->next;
 	}
 	return (i);
 }
