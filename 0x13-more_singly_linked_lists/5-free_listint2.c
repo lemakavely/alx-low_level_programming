19197149588b1ecd6d7c3165e102b422e08d662b
lemakavely <lemakavely@gmail.com>
2022-08-15 15:32:46 -0400

    task 3, 4 and 5 more singly linked list trial 3

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,15 +10,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *next = *head;
+	listint_t *next;
 
 	if (head == NULL)
 		return;
-	while (next != NULL)
+	while (*head != NULL)
 	{
-		temp = next->next;
-		free(next);
-		next = temp;
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-	*head = NULL;
 }
