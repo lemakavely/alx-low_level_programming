13022f33dc244c3b91bdd20ad868dad6a69817be
lemakavely <lemakavely@gmail.com>
2022-08-15 16:10:47 -0400

    task 6 more singly linked list trial 3

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -16,8 +16,8 @@ int pop_listint(listint_t **head)
 
 	if ((*head) == NULL || head == NULL)
 		return (0);
-	new_head = (*head)->next;
-	free(*head);
-	*head = new_head;
+	new_head = *head;
+	*head = new_head->next;
+	free(new_head);
 	return (j);
 }
