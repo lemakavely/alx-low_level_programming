34587718f20790d9ca32c9e2db7bc701100303b5
lemakavely <lemakavely@gmail.com>
2022-08-16 02:43:21 -0400

    task 10 more singly linked list trial 1

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * delete_nodeint_at_index - delete node at an index from linked list
+ * @head: head node of linked list
+ * @index: index of the node to add
+ * Return: the address of the new node
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *next, *tmp, *bfr;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (0);
+	tmp = *head;
+
+	for (i = 0; i < index; i++)
+	{
+		bfr = tmp;
+		tmp = tmp->next;
+		next = tmp->next;
+	}
+	if (tmp == NULL)
+		return (-1);
+	if (i == 0)
+		*head = (*head)->next;
+	else if (next == NULL)
+		bfr->next = NULL;
+	else
+		bfr->next = next;
+	free(tmp);
+	return (1);
+}
