1b3a9c9ad65ad9b5b131e6d1e65aa9e7b094bd37
lemakavely <lemakavely@gmail.com>
2022-08-18 07:41:46 -0400

    task 0 bit manipulation trial 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: pointer to array or binaries
+ *
+ * Return:0 or converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, j = 0, k = 0, m = 1, num = 0;
+
+	while (*(b + i))
+	{
+		if (b[i] == '0' || b[i] == '1')
+			i++;
+		else
+			return (0);
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (b[j] == '1')
+		{
+			while (k < (i - j - 1))
+			{
+				m *= 2;
+				k++;
+			}
+			k = 0;
+			num += m;
+		}
+		m = 1;
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+this is the directory of bit manipulation projects
